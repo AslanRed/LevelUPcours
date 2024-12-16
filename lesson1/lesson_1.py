9e8b8966176f6dcbeceebfb78a1696f7bdb8aaba
AslanRed <aslan.br19@gmail.com>
2024-12-16 21:01:36 +0300

    tasks

--- lesson1/lesson_1.py
+++ lesson1/lesson_1.py
@@ -1,47 +1,47 @@
-# # first task
-# a = int(input('enter first number: '))
-# b = int(input('enter second number: '))
-# z = a + b
-# print(z)
+# first task
+a = int(input('enter first number: '))
+b = int(input('enter second number: '))
+z = a + b
+print(z)
 
-# # second task
-# a = input('enter some number: ')
-# b = int(a)
-# print (type(b))
+# second task
+a = input('enter some number: ')
+b = int(a)
+print (type(b))
 
-# #third task
-# a = int(input('enter first number: '))
-# b = int(input('enter second number: '))
-# z = a - b
-# print(z)
+#third task
+a = int(input('enter first number: '))
+b = int(input('enter second number: '))
+z = a - b
+print(z)
 
-# # fourth task
-# a = float(input('enter first number: '))
-# b = float(input('enter second number: '))
-# c = a * b 
-# z = a / b
-# print(c)
-# print(z)
+# fourth task
+a = float(input('enter first number: '))
+b = float(input('enter second number: '))
+c = a * b 
+z = a / b
+print(c)
+print(z)
 
-# #fifth
-# a = int(input('enter some nuber: '))
-# b = str(a)
-# print(type(b))
+#fifth
+a = int(input('enter some nuber: '))
+b = str(a)
+print(type(b))
 
 #sixth
 a = input('enter True or False: ')
 a != 'False'
 print(a)
 
-# #seventh
-# a = input('enter text: ')
-# b = input('enter second part: ')
-# c = a + ' ' + b
-# print(c)
+#seventh
+a = input('enter text: ')
+b = input('enter second part: ')
+c = a + ' ' + b
+print(c)
 
 
-# #eghth
-# a = input('enter first number:')
-# b = int(input('enter second nuber: '))
-# c = int(a) + b
-# print(c)
+#eghth
+a = input('enter first number:')
+b = int(input('enter second nuber: '))
+c = int(a) + b
+print(c)
