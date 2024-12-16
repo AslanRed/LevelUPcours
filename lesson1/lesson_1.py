01c80c5a5accaa8f05b682be4326bd2b095d0007
AslanRed <aslan.br19@gmail.com>
2024-12-16 20:53:29 +0300

    tasks

---
+++ lesson1/lesson_1.py
@@ -0,0 +1,47 @@
+# first task
+a = int(input('enter first number: '))
+b = int(input('enter second number: '))
+z = a + b
+print(z)
+
+# second task
+a = input('enter some number: ')
+b = int(a)
+print (type(b))
+
+#third task
+a = int(input('enter first number: '))
+b = int(input('enter second number: '))
+z = a - b
+print(z)
+
+# fourth task
+a = float(input('enter first number: '))
+b = float(input('enter second number: '))
+c = a * b 
+z = a / b
+print(c)
+print(z)
+
+#fifth
+a = int(input('enter some nuber: '))
+b = str(a)
+print(type(b))
+
+#sixth
+a = input('enter True or False: ')
+b = a != 'False'
+print(b)
+
+#seventh
+a = input('enter text: ')
+b = input('enter second part: ')
+c = a + ' ' + b
+print(c)
+
+
+#eghth
+a = input('enter first number:')
+b = int(input('enter second nuber: '))
+c = int(a) + b
+print(c)
