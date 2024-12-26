dfad9b177ba85a5626c2ae1034b2339ca806b91c
AslanRed <aslan.br19@gmail.com>
2024-12-26 20:24:19 +0300

    1

--- lesson4/lesson_4.py
+++ lesson4/lesson_4.py
@@ -1,25 +1,25 @@
-# #построить треугольник, где строки содержат от одного до номера строки
-# get_size = input('enter the size: ')
-# size_of_t = int(get_size)
-# res = ''
-# for i in range (1,size_of_t + 1):
-#     res += str(i)
-#     print(res)
+#построить треугольник, где строки содержат от одного до номера строки
+get_size = input('enter the size: ')
+size_of_t = int(get_size)
+res = ''
+for i in range (1,size_of_t + 1):
+    res += str(i)
+    print(res)
 
 
 
-# #2 вывести квадрат из # размером 4*4 из решоток
-# size_of_q = 4
-# while size_of_t > 0:
-#     if size_of_q == 3 or size_of_q == 2:
-#         print('#  #')
-#     else:
-#         print('####')
-#   size_of_q -= 1
+#2 вывести квадрат из # размером 4*4 из решоток
+size_of_q = 4
+while size_of_t > 0:
+    if size_of_q == 3 or size_of_q == 2:
+        print('#  #')
+    else:
+        print('####')
+    size_of_q -= 1
 
 #3 треугольник начиная с 5 звездочек до 1
 brick = '*'
 size = 5
-for i in range(size, 0, -1)
+for i in range(size, 0, -1):
     while size > 0:
         print(brick * size)
