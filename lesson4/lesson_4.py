9b5c6950f3755cb699a280bc45d0e89ca1996505
AslanRed <aslan.br19@gmail.com>
2024-12-26 19:59:41 +0300

    1

---
+++ lesson4/lesson_4.py
@@ -0,0 +1,11 @@
+#построить треугольник, где строки содержат от одного до номера строки
+get_size = input('enter the size: ')
+size_of_t = int(get_size)
+res = ''
+for i in range (1,size_of_t + 1):
+    res += str(i)
+    print(res)
+
+
+
+#2 вывести квадрат из # размером 4*4 из решоток
