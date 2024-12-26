4bbe556b6ed8025ef5e7bce8102d394a4fc0fe09
AslanRed <aslan.br19@gmail.com>
2024-12-26 20:06:23 +0300

    1

--- lesson4/lesson_4.py
+++ lesson4/lesson_4.py
@@ -9,3 +9,9 @@ for i in range (1,size_of_t + 1):
 
 
 #2 вывести квадрат из # размером 4*4 из решоток
+size_of_q = 4
+while size_of_t > 0:
+    if size_of_q == 3 or size_of_q == 2:
+        print('#  #')
+    else:
+        print('####')
